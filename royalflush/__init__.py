91217d8d2904bee173fbf169688a44978702e591
Fran Enguix <enguix.fco@gmail.com>
2025-01-17 01:22:09 +0100

    Bump version: 0.3.0 → 0.3.1

--- royalflush/__init__.py
+++ royalflush/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Francisco Enguix"""
 __email__ = "enguix.fco@gmail.com"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from . import agent, behaviour, dataset, datatypes, log, message, nn, similarity, utils
 
