162fb43acfd23b93da81fbc0e3466c9cefeb1dfa
Fran Enguix <enguix.fco@gmail.com>
2025-01-17 01:14:29 +0100

    Fix royalflush version info.

--- royalflush/__init__.py
+++ royalflush/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Francisco Enguix"""
 __email__ = "enguix.fco@gmail.com"
-__version__ = "0.3.1"
+__version__ = "0.3.0"
 
 from . import agent, behaviour, dataset, datatypes, log, message, nn, similarity, utils
 
